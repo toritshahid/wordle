676e2b70c81b656aa7ef550bf3b4486e050243c3
Syed shahid shabir qadri <53717633+shahidshabir055@users.noreply.github.com>
2022-01-17 21:39:16 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WordleComponent } from './wordle/wordle.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WordleComponent
   ],
   imports: [
     BrowserModule,
